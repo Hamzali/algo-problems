6ebcaf7b8e870264c283022feb6dea86e0677f7b
hamzali <hamzali.tas.95@gmail.com>
2016-05-18 09:54:31 +0300

    carvans/codechef

--- codechef/Carvans.c
+++ codechef/Carvans.c
@@ -7,41 +7,36 @@ void In(int *p);
 int main(){
 
 	int T;
-	In(&T);
+	get_int(&T);
 
 	while(T--){
 
 		int N, result = 0, temp = 0;
 
-		In(&N);
-		In(&temp);
+		get_int(&N);
+		get_int(&temp);
 		N--;
 		result++;
 		while(N--){
 			int S;
-			In(&S);
+			get_int(&S);
 			if(S < temp){
 				result++;
 				temp = S;
 			}
-
 		}
-
 		printf("%d\n", result);
 	}
-
 	return 0;
 }
 
-void In(int *p){
+void get_int(int *p){
 	char c;
 	*p = 0;
-	c = getchar_unlocked();
-	while(c != '\n' && c != ' '){
-		if(c >= '0' && c <= '9'){
-			*p *= 10;
-			*p = *p  + (c - '0');
-		}
+	c = getchar_unlocked(); // For faster input.
+	while(c >= '0' && c <= '9'/*c != '\n' && c != ' '*/){
+		*p *= 10;
+		*p = *p  + (c - '0');
 		c = getchar_unlocked();
 	}
 }
